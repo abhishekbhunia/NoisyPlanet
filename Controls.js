e3bc239054a11bd5af2fda1761b65992d334ac88
abhishekbhunia <abhishekbhunia@gmail.com>
2015-05-20 20:08:59 -0400

    first commit

---
+++ Controls.js
@@ -0,0 +1,32 @@
+
+var Controls = function() {
+
+	
+	var audioParams = {
+		useMic: false,
+		useSample:true,
+		volSens:1,
+		beatHoldTime:8,
+		beatDecayRate:0.95,		
+	};
+
+	function init(){
+
+		//DAT GUI control  
+		gui = new dat.GUI({autoPlace: false });
+		$('#controls').append(gui.domElement);
+		var f2 = gui.addFolder('Settings - Audio');		 
+		f2.add(audioParams, 'volSens', 0, 5).step(0.1).name("Gain");
+		f2.add(audioParams, 'beatHoldTime', 0, 100).step(1).name("Beat Hold");
+		f2.add(audioParams, 'beatDecayRate', 0.9, 1).step(0.01).name("Beat Decay");
+		f2.open();	 
+		 
+		AudioClass.onUseSample();
+
+	}
+
+	return {
+		init:init,
+		audioParams: audioParams,
+	};
+}();
